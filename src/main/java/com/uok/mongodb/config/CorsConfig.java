9520453edf39f54802e73ea6d2cfde4c684b9cd4
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-08 00:34:19 +0530

    feat: add req variables

--- src/main/java/com/uok/mongodb/config/CorsConfig.java
+++ src/main/java/com/uok/mongodb/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.uok.mongodb.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,13 +9,15 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // Allow cookies and authentication
-        config.addAllowedOrigin("http://localhost:5173"); // Allow your frontend's origin
+        config.addAllowedOrigin(frontendUrl); // Allow your frontend's origin
+//        config.addAllowedOriginPattern(${FRONTEND_URL});
         config.addAllowedHeader("*"); // Allow all headers
         config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
         source.registerCorsConfiguration("/**", config);
