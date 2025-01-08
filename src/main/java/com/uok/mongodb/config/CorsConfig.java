e0374763e98ea52ebb9ddcff257b47c3285054d2
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-09 00:01:17 +0530

    chore: Allow all origins

--- src/main/java/com/uok/mongodb/config/CorsConfig.java
+++ src/main/java/com/uok/mongodb/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.uok.mongodb.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,16 +8,18 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true); // Allow cookies and authentication
-        config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("*"); // Allow all origins
         config.addAllowedHeader("*"); // Allow all headers
         config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
-
