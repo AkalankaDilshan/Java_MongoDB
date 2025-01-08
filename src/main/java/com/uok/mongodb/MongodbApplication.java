ec9009b27f6c268f2bca1ed498d1ff57a81a9161
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-08 23:32:08 +0530

    chore: change port to port:8081

--- src/main/java/com/uok/mongodb/MongodbApplication.java
+++ src/main/java/com/uok/mongodb/MongodbApplication.java
@@ -2,6 +2,12 @@ package com.uok.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerException;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class MongodbApplication {
@@ -9,5 +15,4 @@ public class MongodbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbApplication.class, args);
 	}
-
 }
